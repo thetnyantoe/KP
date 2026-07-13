fbba834cfb46a2e6bc4daef25b71ac62539e36cd
ide-dev-05 <yaircube1@gmail.com>
2026-07-13 15:48:03 +0630

    launch7

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,5 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/navigation";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
